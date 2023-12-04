f47b5434682001705ca7916fdedfd48087647618
safwanbin2 <safwanridwan321@gmail.com>
2023-12-04 12:00:16 +0600

    globalerr - zod

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -6,6 +6,7 @@ dotenv.config({
 });
 
 export default {
+  NODE_ENV: process.env.NODE_ENV,
   port: process.env.PORT,
   db_url: process.env.DB_URL,
   default_password: process.env.DEFAULT_PASSWORD,
--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,4 +1,6 @@
 import { NextFunction, Request, Response } from "express";
+import { ZodError, ZodIssue } from "zod";
+import config from "../config";
 
 export const globalErrorHandler = (
   err: any,
@@ -6,9 +8,48 @@ export const globalErrorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  res.status(500).json({
+  let statusCode = err.status || 500;
+  let message = err.message || "Something went wrong";
+
+  type TErrorSource = {
+    path: string | number;
+    message: string;
+  }[];
+
+  let errorSources: TErrorSource = [
+    {
+      path: "",
+      message: "Something went right",
+    },
+  ];
+
+  const handleZodError = (err: ZodError) => {
+    const statusCode = 500;
+    const error = err.issues.map((issue: ZodIssue) => {
+      return {
+        path: issue.path[issue.path.length - 1],
+        message: issue.message,
+      };
+    });
+
+    return {
+      statusCode,
+      message: "Validation Error",
+      errorSources: error,
+    };
+  };
+
+  if (err instanceof ZodError) {
+    const handledError = handleZodError(err);
+    (statusCode = handledError?.statusCode),
+      (message = handledError?.message),
+      (errorSources = handledError?.errorSources);
+  }
+
+  res.status(statusCode).json({
     success: false,
-    message: err.message || "Something Went Wrong !",
-    error: err,
+    message: message,
+    errorSources,
+    stack: config.NODE_ENV === "development" ? err.stack : null,
   });
 };
