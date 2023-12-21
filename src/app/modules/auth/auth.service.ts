f577d5f1becac6e2a9b244a02f8d7190cf93f2ee
safwanbin2 <safwanridwan321@gmail.com>
2023-12-21 12:37:12 +0600

    auth-validations

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,6 +1,21 @@
+import { UserModel } from "../user/user.model";
 import { TAuth } from "./auth.interface";
 
 const logInUserFromDB = async (payload: TAuth) => {
+  // validations
+  const isUserExists = await UserModel.findOne({ id: payload.id });
+  if (!isUserExists) {
+    throw new Error("User does not exists");
+  }
+
+  if (isUserExists.isDeleted) {
+    throw new Error("User is deleted");
+  }
+
+  if (isUserExists.status === "blocked") {
+    throw new Error("User is blocked");
+  }
+
   const result = await payload;
   return result;
 };
