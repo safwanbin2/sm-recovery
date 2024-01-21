470ef958ca8102ece4e7cd887c74edb11aa572b9
safwanbin2 <safwanridwan321@gmail.com>
2024-01-21 22:45:10 +0600

    redo-redux

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(cookieParser());
 
 app.use("/api/v1/users", UserRouter);
